c61852a5e9c27a0ec9d0669ba6389a134ce0ff18
silirama89 <92896189+silirama89@users.noreply.github.com>
2021-11-13 13:24:32 -0500

    pre-media

--- app.js
+++ app.js
@@ -2,150 +2,182 @@
 
 const key = '94e8bd4e-6d31-42b8-b991-02fec8748bd0'
 
-// button variables
+
+// buttons
 
 const nounBtn = document.querySelector('#noun-btn')
 const adjBtn = document.querySelector('#adj-btn')
 const verbBtn = document.querySelector('#verb-btn')
 const advBtn = document.querySelector('#adv-btn')
-const genBtn = [nounBtn, adjBtn, verbBtn, advBtn]
+const clearBtn = document.querySelector('#clear-btn')
 
-// lists variables
-
+// lists
 const nounList = document.querySelector('#noun-list')
 const adjList = document.querySelector('#adj-list')
 const verbList = document.querySelector('#verb-list')
 const advList = document.querySelector('#adv-list')
-const genList = [nounList, adjList, verbList, advList]
-
-// event listeners
-// nounBtn.addEventListener('click', fetchNoun(word))
-// adjBtn.addEventListener('click', fetchAdj(word))
-// verbBtn.addEventListener('click', fetchVerb(word))
-// advBtn.addEventListener('click', fetchAdv(word))
-
 
 // let functionalLabel = ${wordData.fl}
+
+// clear button
+clearBtn.addEventListener("click", function () {
+  let nouns = document.getElementById('noun-list')
+  while (nouns.firstChild) {
+    nouns.removeChild(nouns.firstChild)
+  }
+  let adjs = document.getElementById('adj-list')
+  while (adjs.firstChild) {
+    adjs.removeChild(adjs.firstChild)
+  }
+  const verbs = document.getElementById('verb-list')
+  while (verbs.firstChild) {
+    verbs.removeChild(verbs.firstChild)
+  }
+  const advs = document.getElementById('adv-list')
+  while (advs.firstChild) {
+    advs.removeChild(advs.firstChild)
+  }
+  const txtTitle = document.getElementById('txt-title')
+  txtTitle.value = ''
+  const txtStory = document.getElementById('txt-story')
+  txtStory.value = ''
+})
+
 
 // getting required parameter ("word") through random-word module
 
 import randomWords from 'https://cdn.skypack.dev/random-words';
-
+const word = randomWords()
 
 // event listeners for generator buttons
 
-nounBtn.addEventListener("click", function () {
-  const word = randomWords()
-  fetchNoun(word)
+nounBtn.addEventListener("click", async function () {
+  let foundword = await fetchNoun(word)
+  let forWord = foundword.split(':')[0]
   const newItem = document.createElement('li')
-  newItem.innerText = `${word}`
   nounList.appendChild(newItem)
+  newItem.innerText = forWord
 })
-adjBtn.addEventListener("click", function () {
-  const word = randomWords()
-  fetchAdj(word)
+adjBtn.addEventListener("click", async function () {
+  let foundword = await fetchAdj(word)
+  let forWord = foundword.split(':')[0]
   const newItem = document.createElement('li')
-  newItem.innerText = `${word}`
   adjList.appendChild(newItem)
+  newItem.innerText = forWord
 })
-verbBtn.addEventListener("click", function () {
-  const word = randomWords()
-  fetchVerb(word)
+verbBtn.addEventListener("click", async function () {
+  let foundword = await fetchVerb(word)
+  let forWord = foundword.split(':')[0]
   const newItem = document.createElement('li')
-  newItem.innerText = `${word}`
   verbList.appendChild(newItem)
+  newItem.innerText = forWord
 })
-advBtn.addEventListener("click", function () {
-  const word = randomWords()
-  fetchAdv(word)
+advBtn.addEventListener("click", async function () {
+  let foundword = await fetchAdv(word)
+  let forWord = foundword.split(':')[0]
   const newItem = document.createElement('li')
-  newItem.innerText = `${word}`
   advList.appendChild(newItem)
+  newItem.innerText = forWord
 })
 
-// async fx noun
-
-// function displayWord() {
+// async functions
 
 async function fetchNoun(word) {
+  word = randomWords()
   try {
     let notFound = true;
+    let foundWord
     while (notFound) {
       let res = await axios.get(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?&key=94e8bd4e-6d31-42b8-b991-02fec8748bd0&sort`);
       let foundMatches = res.data;
       console.log(foundMatches)
-      let exactMatch = foundMatches.filter(object => object.indexOf === 0 & object.fl === 'noun')
-      if (typeof exactMatch != 'undefined') {
+      if (foundMatches[0].fl === 'noun') {
+        const match = foundMatches[0]
+        const meta = match.meta
+        const id = meta.id
+        console.log(id)
+        foundWord = id
         notFound = false
       } else {
         word = randomWords()
       }
-    }
+    } return foundWord
   } catch (error) {
     console.log(error);
-  } finally {
-    console.log(word)
   }
 }
 
 async function fetchAdj(word) {
+  word = randomWords()
   try {
     let notFound = true;
+    let foundWord
     while (notFound) {
       let res = await axios.get(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?&key=94e8bd4e-6d31-42b8-b991-02fec8748bd0&sort`);
       let foundMatches = res.data;
       console.log(foundMatches)
-      let exactMatch = foundMatches.filter(object => object.indexOf === 0 & object.fl === 'adjective')
-      if (typeof exactMatch != 'undefined') {
+      if (foundMatches[0].fl === 'adjective') {
+        const match = foundMatches[0]
+        const meta = match.meta
+        const id = meta.id
+        console.log(id)
+        foundWord = id
         notFound = false
       } else {
         word = randomWords()
       }
-    }
+    } return foundWord
   } catch (error) {
     console.log(error);
-  } finally {
-    console.log(word)
   }
 }
 
 async function fetchVerb(word) {
+  word = randomWords()
   try {
     let notFound = true;
+    let foundWord
     while (notFound) {
       let res = await axios.get(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?&key=94e8bd4e-6d31-42b8-b991-02fec8748bd0&sort`);
       let foundMatches = res.data;
       console.log(foundMatches)
-      let exactMatch = foundMatches.filter(object => object.indexOf === 0 & object.fl === 'verb')
-      if (typeof exactMatch != 'undefined') {
+      if (foundMatches[0].fl === 'verb') {
+        const match = foundMatches[0]
+        const meta = match.meta
+        const id = meta.id
+        console.log(id)
+        foundWord = id
         notFound = false
       } else {
         word = randomWords()
       }
-    }
+    } return foundWord
   } catch (error) {
     console.log(error);
-  } finally {
-    console.log(word)
   }
 }
+
 async function fetchAdv(word) {
+  word = randomWords()
   try {
     let notFound = true;
+    let foundWord
     while (notFound) {
       let res = await axios.get(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?&key=94e8bd4e-6d31-42b8-b991-02fec8748bd0&sort`);
       let foundMatches = res.data;
       console.log(foundMatches)
-      let exactMatch = foundMatches.filter((element, 0) => element.fl === 'adverb')
-      if (typeof exactMatch != 'undefined') {
+      if (foundMatches[0].fl === 'adverb') {
+        const match = foundMatches[0]
+        const meta = match.meta
+        const id = meta.id
+        console.log(id)
+        foundWord = id
         notFound = false
       } else {
         word = randomWords()
       }
-    }
+    } return foundWord
   } catch (error) {
     console.log(error);
-  } finally {
-    console.log(word)
   }
 }
